5a549eefddcc9d6554be1e6d1e96adb1c131a0e6
Yamin Mansuri <mansuriyamin111@gmail.com>
2021-05-24 19:13:14 +0530

    Calculator Completed

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const outputScreen = document.getElementById("output-screen");
 
-function display(num) {
-	outputScreen.value += num;
-}
+const display = (num) => {
+  outputScreen.value += num;
+};
 
-function calculate() {
-	outputScreen.value = eval(outputScreen.value);
-}
+const calculate = () => {
+  try {
+    outputScreen.value = eval(outputScreen.value);
+  } catch (err) {
+    alert("Invalid equation!");
+  }
+};
+
+const allClear = () => {
+  outputScreen.value = "";
+};
+
+const del = () => {
+  outputScreen.value = outputScreen.value.slice(0, -1);
+};
